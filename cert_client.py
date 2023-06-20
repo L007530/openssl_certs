bee76e56fff82f11b30f78f4522b829aeb1cf3ce
BillyZhang <zhang_yuchun@network.lilly.com>
2023-06-20 15:10:25 +0800

    Update cert_client.py

--- cert_client.py
+++ cert_client.py
@@ -5,23 +5,25 @@ import argparse
 ap = argparse.ArgumentParser()
 # ap.add_argument("-d", "--debug", required=False, default='0',
 #                 help="enable debug mode")
-ap.add_argument("-p", "--path", default="cert_files", required=False,
+ap.add_argument("-f", "--folder", default="cert_files", required=False,
                 help="path to store cert files")
 ap.add_argument("-u", "--url", default="example.lilly.com", required=False,
                 help="url for certificate")
+ap.add_argument("-c", "--passphrase", default="PASSPHRASE", required=False,
+                help="ca.key passphrase")
 ap.add_argument("-m", "--method", default="key+csr", required=False,
                 help="create key+csr or key+csr+cert")
 args = vars(ap.parse_args())
 
 
 class Client(cert.Certificate):
-    def __init__(self, path, cert_url, self_signed_days=45):
-        super().__init__(path, cert_url, self_signed_days)
+    def __init__(self, path, cert_url, passphrase, self_signed_days=45):
+        super().__init__(path, cert_url, passphrase, self_signed_days)
 
     def create_key_csr(self):
         # create private key
         p_key = self.generate_key()
-        print(f"Encrypted Private key: ca.key is created under: {self.path}, passcode: lilly123")
+        print(f"Encrypted Private key: ca.key is created under: {self.path}, passphrase: {args['passphrase']}")
         print(f"Non-encrypted Private key: key.pem is created under: {self.path}")
 
         # create public key
@@ -30,7 +32,7 @@ class Client(cert.Certificate):
 
         # create readme.txt
         self.generate_readme(line=f"{self.cert_url}\n")
-        self.generate_readme(line="ca.key - Encrypted Private key, passcode: lilly123")
+        self.generate_readme(line=f"ca.key - Encrypted Private key, passphrase: {args['passphrase']}")
         self.generate_readme(line="key.pem - Non-encrypted Private key")
         self.generate_readme(line="csr.pem - Self-signed Public key")
         print(f"Readme file is created under: {self.path}")
@@ -51,7 +53,7 @@ class Client(cert.Certificate):
 
         # create readme.txt
         self.generate_readme(line=f"{self.cert_url}\n")
-        self.generate_readme(line="ca.key - Encrypted Private key, passcode: lilly123")
+        self.generate_readme(line=f"ca.key - Encrypted Private key, passphrase: {args['passphrase']}")
         self.generate_readme(line="key.pem - Non-encrypted Private key")
         self.generate_readme(line="csr.pem - Self-signed Public key")
         self.generate_readme(line="certificate.pem - Self-signed Certificate")
@@ -59,10 +61,12 @@ class Client(cert.Certificate):
 
 
 if __name__ == "__main__":
-    dir_path = args['path']
+    dir_path = args['folder']
     cert_url = args['url']
+    passphrase = Client.convert_str_to_bytestr(args['passphrase'])
+    # print(passphrase)
 
-    client = Client(path=dir_path, cert_url=cert_url)
+    client = Client(path=dir_path, cert_url=cert_url, passphrase=passphrase)
     if args['method'] == 'key+csr':
         client.create_key_csr()
     elif args['method'] == 'key+csr+cert':
