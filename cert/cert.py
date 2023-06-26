cc5299c4dd4c24814e790f2000308edab6b2f0f7
BillyZhang <zhang_yuchun@network.lilly.com>
2023-06-26 10:22:58 +0800

    Update cert.py

--- cert/cert.py
+++ cert/cert.py
@@ -6,6 +6,8 @@ from cryptography.hazmat.primitives import hashes
 
 import datetime
 import os
+import random
+import string
 
 
 class Certificate:
@@ -24,6 +26,38 @@ class Certificate:
         elif os.path.exists(path):
             # print(f'Existed path: {path}, would not create')
             return path
+
+    @staticmethod
+    def generate_password(length=8, exclude_chars='#-'):
+        # 确保密码长度至少为 4，以满足包含大写字母、小写字母、数字和特殊字符的要求
+        if length < 4:
+            print("Password length must be at least 4")
+            return None
+
+        # 定义字符集，移除排除的字符
+        uppercase_letters = string.ascii_uppercase
+        lowercase_letters = string.ascii_lowercase
+        digits = string.digits
+        special_characters = string.punctuation.translate({ord(c): None for c in exclude_chars})
+
+        # 随机选择一个大写字母作为密码的第一个字符
+        password = [random.choice(uppercase_letters)]
+
+        # 随机选择一个数字、一个小写字母和一个特殊字符
+        password.append(random.choice(digits))
+        password.append(random.choice(lowercase_letters))
+        password.append(random.choice(special_characters))
+
+        # 填充剩下的位置，以随机的字符
+        all_characters = uppercase_letters + lowercase_letters + digits + special_characters
+        for i in range(length - 4):
+            password.append(random.choice(all_characters))
+
+        # 打乱密码中的字符顺序
+        random.shuffle(password[1:])
+
+        # 将字符列表转换为字符串
+        return ''.join(password)
 
     @staticmethod
     def convert_str_to_bytestr(str_inputed):
@@ -128,8 +162,15 @@ if __name__ == "__main__":
     dir_path = 'test123.lilly.com'
     cert_url = u'test123.lilly.com'
 
-    cert = Certificate(path=dir_path, cert_url=cert_url)
-    p_key = cert.generate_key()
-    cert.generate_csr(key=p_key)
-    cert.generate_self_signed_cert(key=p_key)
+    # cert = Certificate(path=dir_path, cert_url=cert_url)
+    # p_key = cert.generate_key()
+    # cert.generate_csr(key=p_key)
+    # cert.generate_self_signed_cert(key=p_key)
+    passphrase1=Certificate.generate_password()
+    print(passphrase1)
+    passphrase2 = Certificate.generate_password(12)
+    print(passphrase2)
+    passphrase3 = Certificate.generate_password(10, '%&')
+    print(passphrase3)
+
     pass
